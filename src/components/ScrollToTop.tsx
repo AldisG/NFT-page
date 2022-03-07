30a0ba8e47a20459dccea96f54a00c94b51bc6c0
whyimsobroke <aldisgailis1@gmail.com>
2022-03-07 13:53:08 -0800

    added arrow button anim

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,5 +1,13 @@
 import React, { useState } from 'react';
 import { BsChevronUp } from '../util/importedIcons';
+import { motion } from 'framer-motion';
+
+const bouncyArrow = {
+  start: { y: 0 },
+  end: { y: 10 },
+  transition: { yoyo: Infinity, duration: 1.5 },
+};
+
 const ScrollToTop = () => {
   const [visibleBar, setVisibleBar] = useState(false);
   window.addEventListener('scroll', () => {
@@ -8,7 +16,14 @@ const ScrollToTop = () => {
   return (
     <div className={`scroll-top ${!visibleBar ? 'invisible-bar' : ''}`}>
       <a href="#" className="sign-up-button scroll-top-btn">
-        <BsChevronUp />
+        <motion.div
+          initial={bouncyArrow.start}
+          animate={bouncyArrow.end}
+          draggable={true}
+          transition={bouncyArrow.transition}
+        >
+          <BsChevronUp className="animated-icon" />
+        </motion.div>
       </a>
     </div>
   );
